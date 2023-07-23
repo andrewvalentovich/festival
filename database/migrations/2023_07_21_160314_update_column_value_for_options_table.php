f565cff68d991eceb3ceab8aa7be088ac7ca4ba4
Andrew Valentovich <andrewvalentovich14@gmail.com>
2023-07-23 23:16:23 +0300

    fix: Migration for options table

--- database/migrations/2023_07_21_160314_update_column_value_for_options_table.php
+++ database/migrations/2023_07_21_160314_update_column_value_for_options_table.php
@@ -11,7 +11,10 @@ return new class extends Migration
      */
     public function up(): void
     {
-        //
+        Schema::table('options', function (Blueprint $table) {
+            $table->dropColumn('value');
+            $table->text('value');
+        });
     }
 
     /**
@@ -19,6 +22,9 @@ return new class extends Migration
      */
     public function down(): void
     {
-        //
+        Schema::table('options', function (Blueprint $table) {
+            $table->dropColumn('value');
+            $table->string('value');
+        });
     }
 };
