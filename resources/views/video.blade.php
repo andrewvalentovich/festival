2c58b381936ace4b22d7ce897a4f74b0b927c067
Andrew Valentovich <andrewvalentovich14@gmail.com>
2024-04-02 16:36:39 +0300

    fix

--- resources/views/video.blade.php
+++ resources/views/video.blade.php
@@ -2,11 +2,207 @@
 
 @section('title')Видео@endsection
 @section('content')
+{{--    <div class="type__about-title type__title">--}}
+{{--        Видео--}}
+{{--    </div>--}}
+
     <div class="type__about-title type__title">
         Видео
     </div>
-    <div class="type__about-video">
-        <video style="width:100%; height:auto" controls loop src="https://videon.img.ria.ru/Out/Flv/direct/2023_10_06_MMPC202310061300ZAL32_cyoh0gty.2m5.mp4"></video>
+{{--    <div class="type__about-video">--}}
+{{--        <video style="width:100%; height:auto" controls loop src="https://cloud.mail.ru/public/QpeC/ZfBCJemnn"></video>--}}
 {{--        <iframe style="width: 100%" height="315" src="https://www.youtube.com/embed/av9qnHKieZs" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>--}}
+{{--    </div>--}}
+    <div class="type__news-list">
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        6  октября 2023, 13:00
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Москва, Стеклянный зал, Пресс-конференция
+                    Пресс-конференция, посвященная открытию I Всероссийского фестиваля "Народный Рахманинов
+                </p>
+                <a target="_blank" href="https://pressria.ru/20231006/955544233.html?ysclid=lugkiw6anq449326626" class="type__lead">
+                    https://pressria.ru/20231006/955544233.html?ysclid=lugkiw6anq449326626
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        13 октября 2023 г.
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Прямая трансляция первого гала-концерта
+                    Первого Всероссийского фестиваля
+                    «Народный Рахманинов»
+                    «Филармония-2». Концертный зал имени С.В. Рахманинова
+                </p>
+                <a target="_blank" href="https://www.culture.ru/live/movies/65587/gala-koncert-pervogo-vserossii-skogo-festivalya-narodnyi-rakhmaninov?ysclid=lugk9n4v13708196297" class="type__lead">
+                    https://www.culture.ru/live/movies/65587/gala-koncert-pervogo-vserossii-skogo-festivalya-narodnyi-rakhmaninov?ysclid=lugk9n4v13708196297
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        15 октября 2023 г.
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Прямая трансляция второго гала-концерта
+                    Первого Всероссийского фестиваля
+                    «Народный Рахманинов»
+                    «Филармония-2». Концертный зал имени С.В. Рахманинова
+                </p>
+                <a target="_blank" href="https://www.culture.ru/live/broadcast/65727/vtoroi-gala-koncert-vserossii-skogo-festivalya-narodnyi-rakhmaninov?ysclid=lugkfq7qky883119111" class="type__lead">
+                    https://www.culture.ru/live/broadcast/65727/vtoroi-gala-koncert-vserossii-skogo-festivalya-narodnyi-rakhmaninov?ysclid=lugkfq7qky883119111
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        8 ноября 2023, 11:00
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Москва, Стеклянный зал, Пресс-конференция
+                    Итоги I Всероссийского фестиваля "Народный Рахманинов"
+                </p>
+                <a target="_blank" href="https://pressria.ru/20231108/955730809.html" class="type__lead">
+                    https://pressria.ru/20231108/955730809.html
+                </a>
+            </div>
+        </div>
+
+        <div class="type__about-title type__title">
+            Дневники Первого Всероссийского фестиваля «Народный Рахманинов»
+        </div>
+
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        1 День
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/QpeC/ZfBCJemnn" class="type__lead">
+                    https://cloud.mail.ru/public/QpeC/ZfBCJemnn
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        2 День
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/kRhE/L4qv7CvWR" class="type__lead">
+                    https://cloud.mail.ru/public/kRhE/L4qv7CvWR
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        3 День
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/VseL/tz6SijHgm" class="type__lead">
+                    https://cloud.mail.ru/public/VseL/tz6SijHgm
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        4 День
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/N5hF/DC3his8RH" class="type__lead">
+                    https://cloud.mail.ru/public/N5hF/DC3his8RH
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        5 День
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/8HYc/uZD8D8azh" class="type__lead">
+                    https://cloud.mail.ru/public/8HYc/uZD8D8azh
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        6 День
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/6ci5/8xh94vFCy" class="type__lead">
+                    https://cloud.mail.ru/public/6ci5/8xh94vFCy
+                </a>
+            </div>
+        </div>
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__date" style="margin-bottom: 4px;">
+                    <span>
+                        7 День
+                    </span>
+                </p>
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/qc74/Ha1VcwvQ4" class="type__lead">
+                    https://cloud.mail.ru/public/qc74/Ha1VcwvQ4
+                </a>
+            </div>
+        </div>
+
+        <div class="type__news-item ">
+            <div class="type__news-item-text">
+                <p class="type__subtitle" style="margin-bottom: 16px;">
+                    Фильм первого Всероссийского фестиваля «Народный Рахманинов»
+                </p>
+                <a target="_blank" href="https://cloud.mail.ru/public/DcU5/EkV3F35Pc" class="type__lead">
+                    https://cloud.mail.ru/public/DcU5/EkV3F35Pc
+                </a>
+            </div>
+        </div>
+
     </div>
 @endsection
